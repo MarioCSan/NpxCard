94bdd11f5744b0a84a96683db4f039c09fba1ae7
Mario <canalessanchezmario@gmail.com>
2024-02-20 19:20:42 +0100

    Changes in index to try to make a loop until the user exit the app

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
 
-import inquirer from 'inquirer'
-import { me, questions } from './src/content.js'
-import { clear } from 'console'
-clear()
+import inquirer from "inquirer";
+import { me, questions } from "./src/content.js";
+import { clear } from "console";
+clear();
 
-console.log(me)
+console.log(me);
 
-const prompt = inquirer.createPromptModule()
-prompt(questions).then(
-    answer => {
-        while (answer!=='exit')
-            answer.action()
-    });
+const prompt = inquirer.createPromptModule();
+
+const runPrompt = () => {
+  prompt(questions).then((answer) => {
+    answer.action();
+    if (answer.action !== questions[0].choices[2].value) {
+        runPrompt();
+      }
+  });
+};
+
+runPrompt();
