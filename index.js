a68b9812b158256a36ee0353bfdea737ea049cbb
Mario <canalessanchezmario@gmail.com>
2024-02-21 12:33:38 +0100

    Changes to support multi-languaje, changes in contact information

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
-import { me, questions } from "./src/content.js";
+import { me, questions } from "./src/en_content.js";
 import { clear } from "console";
 clear();
-
+ 
 console.log(me);
 
 const prompt = inquirer.createPromptModule();
@@ -12,7 +12,7 @@ const prompt = inquirer.createPromptModule();
 const runPrompt = () => {
   clear();
   console.log(me);
-  prompt(questions).then((answer) => {
+  prompt(questions).then(answer => {
     answer.action();
     if (answer.action !== questions[0].choices[2].value) {
       prompt(questions);
--- src/data.js
+++ src/data.js
@@ -4,11 +4,13 @@ export const data = {
   name: chalk.bold.white('Mario Canales'),
   tagline: chalk.bold.grey('.NET, Azure, Angular and React developer'),
   location: chalk.bold.grey('Madrid, Spain'),
-  github: chalk.white('https://github.com/marioCSan/'),
-  linkedin: chalk.hex('#0077B5')('https://linkedin.com/in/mariocanalessanchez'),
-  website: chalk.white('https://mariocanales.es/'),
+  github: chalk.hex('#0077B5')('  https://github.com/marioCSan/'),
+  linkedin: chalk.white('https://linkedin.com/in/mariocanalessanchez'),
+  website: chalk.white(' https://mariocanales.es/'),
+  dev: chalk.hex('#0077B5')('  https://dev.to/mariocsan/'),
   labelWebsite: chalk.white.bold('Website:'),
   labelTwitter: chalk.hex('#08a0e9').bold('Twitter:'),
-  labelGitHub: chalk.hex('#F6F6F6').bold('GitHub:'),
-  labelLinkedin: chalk.hex('#0077B5').bold('Linkedin:')
+  labelGitHub: chalk.hex('#0077B5').bold('GitHub:'),
+  labelLinkedin: chalk.hex('#F6F6F6').bold('Linkedin:'),
+  labelDev: chalk.hex('#F6F6F6').bold('Dev.to:')
 }
